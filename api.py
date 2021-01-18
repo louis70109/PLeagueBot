bc7972331c2312dcb5af1b6869302e18eb4876fa
louis70109 <louis70109@gmail.com>
2021-01-18 22:53:05 +0800

    feat: share target picker

--- api.py
+++ api.py
@@ -3,15 +3,18 @@ from flask_cors import CORS
 from flask_restful import Api
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 from controller.line_controller import LineController
+from controller.liff_controller import LiffController
 
 app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
 api.add_resource(LineController, '/webhooks/line')
+api.add_resource(LiffController, '/liff/share')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ controller/liff_controller.py
@@ -0,0 +1,45 @@
+import os
+from flask import request, render_template, Response
+from flask_restful import Resource
+
+from utils.db import find_game, find_stream
+from utils.flex import game_flex_template, stream_flex_template
+
+LIFF_ID = os.getenv('LIFF_SHARE_ID')
+
+
+class LiffController(Resource):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get(self):
+        if request.args.get('liff.state'):
+            return Response(render_template('liff_redirect.html', liff_id=LIFF_ID))
+        game = request.args.get('game')
+        stream = request.args.get('stream')
+        content = []
+        alt = '與您分享'
+        if game:
+            alt = "分享 P+ 聯盟賽程給你"
+            row = find_game(game)
+
+            content.append(game_flex_template(
+                row['id'],
+                row['customer_image'],
+                row['main_image'],
+                row['score'],
+                row['people'],
+                row['place'],
+                row['event_date'])
+            )
+        elif stream:
+            alt = "分享一個 P+ 影片給你"
+            row = find_stream(stream)
+            content.append(stream_flex_template(row['id'], row['title'], row['image'], row['link']))
+            
+        msg = {"type": "flex", "altText": alt, "contents": {**{
+            "type": "carousel",
+            "contents": content
+        }}}
+        return Response(render_template('share_message.html', flex=msg, liff_id=LIFF_ID))
