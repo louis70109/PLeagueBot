bd9c3331d308c6cf8bbdd2a14b482bd1b6aea979
louis70109 <louis70109@gmail.com>
2021-03-14 00:55:53 +0800

    support pgsql in docker & add 500 tracing

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
 FROM python:3.6-alpine
+COPY requirements.txt .
+
+RUN \
+ apk add --no-cache postgresql-libs && \
+ apk add --no-cache --virtual .build-deps gcc musl-dev postgresql-dev && \
+ python3 -m pip install -r requirements.txt --no-cache-dir && \
+ apk --purge del .build-deps
+
 
 ADD . /code
 WORKDIR /code
 RUN pip install -r requirements.txt
 
-CMD ["python", "app.py"]
+CMD ["python", "api.py"]
--- api.py
+++ api.py
@@ -1,10 +1,15 @@
-from flask import Flask
+import os
+import traceback
+
+from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Api
-from dotenv import load_dotenv
+from lotify.client import Client
 
+if os.getenv('PY_ENV') != 'production':
+    from dotenv import load_dotenv
 
-load_dotenv()
+    load_dotenv()
 
 from controller.line_controller import LineController
 from controller.liff_controller import LiffController
@@ -13,8 +18,24 @@ app = Flask(__name__)
 CORS(app)
 
 api = Api(app)
+
 api.add_resource(LineController, '/webhooks/line')
 api.add_resource(LiffController, '/liff/share')
+
+
+@app.errorhandler(500)
+def internal_error(e):
+    error_trace = str(traceback.format_exc())[-300:]
+    msg = 'P+ bot: url: ' + str(request.url) + " inbound: " + str(request.remote_addr) + " log: " + error_trace
+    lotify = Client()
+    lotify.send_message(access_token=os.getenv('LINE_NOTIFY_TOKEN'), message=msg)
+    return "500"
+
+
+@app.route("/health", methods=['GET'])
+def health_check():
+    return 'ok'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
