2b4bc5ae13cb3abb8fd3723e00aa2bed3a45de48
NiJia Lin <louis70109@gmail.com>
2021-03-17 23:50:15 +0800

    feat: news and shop model

--- models/news.py
+++ models/news.py
@@ -5,24 +5,26 @@ from models.database import db
 shop_seq = Sequence('stream_id_seq')
 
 
-class Shop(db.Model):
-    __tablename__ = 'shop'
+class News(db.Model):
+    __tablename__ = 'news'
     id = db.Column(db.Integer, shop_seq, primary_key=True, server_default=shop_seq.next_value())
     link = db.Column(db.String(255))
     image = db.Column(db.String(255))
-    product = db.Column(db.String(255))
-    price = db.Column(db.String(12))
-    __table_args__ = (db.UniqueConstraint('product', name='product_unique'),)
+    date = db.Column(db.String(12))
+    tag = db.Column(db.String(25))
+    description = db.Column(db.String(100))
+    __table_args__ = (db.UniqueConstraint('description', name='desc_unique'),)
 
     def __repr__(self):
         return f"<Stream (id={self.id}, link={self.link}, " \
-               f"image={self.image}, product={self.product}, " \
-               f"price={self.price}>"
+               f"image={self.image}, date={self.date}, " \
+               f"tag={self.tag}, description={self.description}>"
 
-    def __init__(self, id, link, image, product, price, **kwargs):
-        super(Shop, self).__init__(**kwargs)
+    def __init__(self, id, link, image, date, tag, description, **kwargs):
+        super(News, self).__init__(**kwargs)
         self.id = id
         self.link = link
         self.image = image
-        self.product = product
-        self.price = price
+        self.date = date
+        self.tag = tag
+        self.description = description
--- models/shop.py
+++ models/shop.py
@@ -2,27 +2,27 @@ from sqlalchemy import Sequence
 
 from models.database import db
 
-stream_seq = Sequence('stream_id_seq')
+shop_seq = Sequence('stream_id_seq')
 
 
-class Stream(db.Model):
-    __tablename__ = 'stream'
-    id = db.Column(db.Integer, stream_seq, primary_key=True, server_default=stream_seq.next_value())
+class Shop(db.Model):
+    __tablename__ = 'shop'
+    id = db.Column(db.Integer, shop_seq, primary_key=True, server_default=shop_seq.next_value())
     link = db.Column(db.String(255))
     image = db.Column(db.String(255))
-    title = db.Column(db.String(100))
-    is_live = db.Column(db.Boolean(), default=True)
-    __table_args__ = (db.UniqueConstraint('link', 'image', 'title', name='stream_unique'),)
+    product = db.Column(db.String(255))
+    price = db.Column(db.String(12))
+    __table_args__ = (db.UniqueConstraint('product', name='product_unique'),)
 
     def __repr__(self):
         return f"<Stream (id={self.id}, link={self.link}, " \
-               f"image={self.image}, tile={self.tile}, " \
-               f"is_live={self.is_live}>"
+               f"image={self.image}, product={self.product}, " \
+               f"price={self.price}>"
 
-    def __init__(self, id, link, image, tile, is_live, **kwargs):
-        super(Stream, self).__init__(**kwargs)
+    def __init__(self, id, link, image, product, price, **kwargs):
+        super(Shop, self).__init__(**kwargs)
         self.id = id
         self.link = link
         self.image = image
-        self.tile = tile
-        self.is_live = is_live
+        self.product = product
+        self.price = price
