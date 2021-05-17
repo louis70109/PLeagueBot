2c0b44ccbb7b20511f5a323f45a674fd31f37a8c
louis70109 <louis70109@gmail.com>
2021-05-17 23:55:36 +0800

    import fastapi. bot api on-going. liff just uses Flask currently

--- api.py
+++ api.py
@@ -1,37 +1,45 @@
 import os
-import traceback
 
-from flask import Flask, request
-from flask_cors import CORS
-from flask_restful import Api
-from lotify.client import Client
-
-from models.database import db
 
 if os.getenv('FLASK_ENV') != 'production':
     from dotenv import load_dotenv
 
     load_dotenv()
 
-from controller.line_controller import LineController
-from controller.liff_controller import LiffController
+import traceback
+import uvicorn
+from fastapi import FastAPI
+from flask import Flask, request, Response, render_template
+from flask_cors import CORS
+from lotify.client import Client
 
-app = Flask(__name__)
-CORS(app)
+from fastapi.middleware.wsgi import WSGIMiddleware
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-with app.app_context():
+from models.database import db
+
+from controller import line_controller
+from controller.liff_controller import liff_share_controller
+
+app = FastAPI()
+
+app.include_router(line_controller.router)
+
+
+flask_app = Flask(__name__)
+CORS(flask_app)
+
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(flask_app)
+with flask_app.app_context():
     db.create_all()
 
-api = Api(app)
+# api = Api(flask_app)
 
-api.add_resource(LineController, '/webhooks/line')
-api.add_resource(LiffController, '/liff/share')
+# api.add_resource(LineController, '/webhooks/line')
 
 
-@app.errorhandler(500)
+@flask_app.errorhandler(500)
 def internal_error(e):
     error_trace = str(traceback.format_exc())[-300:]
     msg = 'P+ bot: url: ' + str(request.url) + \
@@ -41,10 +49,23 @@ def internal_error(e):
     return "500"
 
 
-@app.route("/health", methods=['GET'])
+@flask_app.route("/health", methods=['GET'])
 def health_check():
     return 'ok'
 
 
+@flask_app.route("/liff/share", methods=['GET'])
+def liff_page():
+    flex, liff_id = liff_share_controller()
+
+    if flex and liff_id:
+        return Response(render_template('share_message.html', flex=flex, liff_id=liff_id))
+    else:
+        return Response(render_template('liff_redirect.html', liff_id=liff_id))
+
+
+
+app.mount("/", WSGIMiddleware(flask_app))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    uvicorn.run("api:app", host='0.0.0.0', port=5000, reload=True)
--- controller/liff_controller.py
+++ controller/liff_controller.py
@@ -1,6 +1,5 @@
 import os
-from flask import request, render_template, Response
-from flask_restful import Resource
+from flask import request
 
 from models.game import Game
 from models.news import News
@@ -13,50 +12,46 @@ from utils.flex import game_flex_template, stream_flex_template, news_flex_templ
 LIFF_ID = os.getenv('LIFF_SHARE_ID')
 
 
-class LiffController(Resource):
+def liff_share_controller():
+    if request.args.get('liff.state'):
+        return None, LIFF_ID
+    game = request.args.get('game')
+    stream = request.args.get('stream')
+    news = request.args.get('news')
+    shop = request.args.get('shop')
+    content = []
+    alt = '你已被標註。'
+    if game:
+        alt = "分享 P+ 聯盟賽程給你"
+        row = Game.query.filter_by(id=game).first()
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        content.append(game_flex_template(
+            row.id,
+            row.customer_image,
+            row.main_image,
+            row.score,
+            row.people,
+            row.place,
+            row.event_date)
+        )
+    elif stream:
+        alt = "分享一個 P+ 影片給你"
+        row = Stream.query.filter_by(id=stream).first()
+        content.append(stream_flex_template(row.id, row.title, row.image, row.link))
+    elif news:
+        alt = "我找到一篇 P+ 的新聞啦！！"
+        row = News.query.filter_by(id=news).first()
+        content.append(news_flex_template(row))
+    elif shop:
+        alt = "分享個 P+ 商品給你～"
+        row = Shop.query.filter_by(id=shop).first()
+        content.append(shop_flex_template(row))
+    else:
+        content = [add_me()]
 
-    def get(self):
-        if request.args.get('liff.state'):
-            return Response(render_template('liff_redirect.html', liff_id=LIFF_ID))
-        game = request.args.get('game')
-        stream = request.args.get('stream')
-        news = request.args.get('news')
-        shop = request.args.get('shop')
-        content = []
-        alt = '你已被標註。'
-        if game:
-            alt = "分享 P+ 聯盟賽程給你"
-            row = Game.query.filter_by(id=game).first()
+    msg = {"type": "flex", "altText": alt, "contents": {**{
+        "type": "carousel",
+        "contents": content
+    }}}
+    return msg, LIFF_ID
 
-            content.append(game_flex_template(
-                row.id,
-                row.customer_image,
-                row.main_image,
-                row.score,
-                row.people,
-                row.place,
-                row.event_date)
-            )
-        elif stream:
-            alt = "分享一個 P+ 影片給你"
-            row = Stream.query.filter_by(id=stream).first()
-            content.append(stream_flex_template(row.id, row.title, row.image, row.link))
-        elif news:
-            alt = "我找到一篇 P+ 的新聞啦！！"
-            row = News.query.filter_by(id=news).first()
-            content.append(news_flex_template(row))
-        elif shop:
-            alt = "分享個 P+ 商品給你～"
-            row = Shop.query.filter_by(id=shop).first()
-            content.append(shop_flex_template(row))
-        else:
-            content = [add_me()]
-
-        msg = {"type": "flex", "altText": alt, "contents": {**{
-            "type": "carousel",
-            "contents": content
-        }}}
-        return Response(render_template('share_message.html', flex=msg, liff_id=LIFF_ID))
