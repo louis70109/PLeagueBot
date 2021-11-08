ccc2bea8aaedda6a41cb58c4b688660a5916e991
louis70109 <nijia.lin@linecorp.com>
2021-11-08 17:47:53 +0800

    update api.py

--- api.py
+++ api.py
@@ -57,4 +57,4 @@ def liff_page():
 app.mount("/", WSGIMiddleware(flask_app))
 
 if __name__ == '__main__':
-    uvicorn.run("api:app", host='0.0.0.0', port=5000, reload=True)
+    uvicorn.run("api:app", host='0.0.0.0', port=5000)
